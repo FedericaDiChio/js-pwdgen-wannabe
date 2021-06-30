f70252831b9ff624ee184cda10aa982c7b5d6e96
FedericaDiChio <federica.dichio7@gmail.com>
2021-07-01 00:16:04 +0200

    aggiunti commenti

--- Script/script.js
+++ Script/script.js
@@ -1,20 +1,33 @@
 // console.log("JS OK");
 
-/* Chiedi all’utente il suo nome,
+
+/* 
+ESERCIZIO:
+Chiedi all’utente il suo nome,
 poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito21 */
 
 
+/* STEP:
+1. Creare le variabili con nome, cognome, colore preferito; Creare variabili messaggio per utente e numero da assegnare alla password;
+2. Recupero elemento HTML -> assegnare variabile al document.getElement;
+3. Creare i prompt per nome, cognome e colore preferito;
+4. Console.log per ogni variabile;
+5. Inserire le variabili concatenate per fornire la password all'utente; 
+
+*/
+
+
 // Variabili - assegnazione e dichiarazione
 
 var message = "La tua password è:";
+var numberPw = "21";
 var yourpwPlaceholder = document.getElementById("yourpw");
 
 var userName = prompt("Come ti chiami?");
 var userSurname = prompt("Quale è il tuo cognome?");
 var userColor = prompt("Quale è il tuo colore preferito?");
-var numberPw = "21";
 console.log ("Nome: ", userName);
 console.log ("Cognome: ", userSurname);
 console.log ("Colore preferito: ", userColor);
