9ef566336fd3368b5745325dd0c72a7644b94ebf
FedericaDiChio <federica.dichio7@gmail.com>
2021-06-30 16:04:51 +0200

    aggiunti console.log

--- Script/script.js
+++ Script/script.js
@@ -9,17 +9,17 @@ Infine scrivi sulla pagina nomecognomecolorepreferito21 */
 // Variabili - assegnazione e dichiarazione
 
 var messagepw = "La tua password è:";
-var yourpwplaceHolder = document.getElementById("yourpw");
-console.log("yourpwplaceHolder.innerHTML");
+var yourpwPlaceholder = document.getElementById("yourpw");
 
 var name = prompt("Come ti chiami?");
 var surname = prompt("Quale è il tuo cognome?");
 var favColor = prompt("Quale è il tuo colore preferito?");
-console.log ("name");
-console.log ("surname");
-console.log ("favColor");
+console.log ("Nome: ", name);
+console.log ("Cognome: ", surname);
+console.log ("Colore preferito: ", favColor);
+console.log ("Elemento HTML: ", yourpwPlaceholder);
 
 
 // Scriviamo la password dell'utente 
 
-yourpwplaceHolder.innerHTML = messagepw + " " + name + surname + favColor + 21;
+yourpwPlaceholder.innerHTML = messagepw + " " + name + surname + favColor + 21;
