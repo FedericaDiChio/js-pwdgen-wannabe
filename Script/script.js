820d636bc034199de9e0e46b77cf294362ad4d01
FedericaDiChio <federica.dichio7@gmail.com>
2021-06-30 15:32:30 +0200

    Dichiarazione e assegnazione variabili

---
+++ Script/script.js
@@ -0,0 +1,19 @@
+console.log("JS OK");
+
+/* Chiedi all’utente il suo nome,
+poi chiedi il suo cognome,
+poi chiedi il suo colore preferito
+Infine scrivi sulla pagina nomecognomecolorepreferito21 */
+
+
+// Variabili - assegnazione e dichiarazione
+
+var messagepw = prompt("La tua password è");
+var name = prompt("Come ti chiami?");
+var surname = prompt("Quale è il tuo cognome?");
+var favColor = prompt("Quale è il tuo colore preferito?")
+console.log ("name");
+console.log ("surname");
+console.log ("favColor");
+var yourpwplaceHolder = document.getElementById("yourpw");
+console.log("yourpwplaceHolder.innerHTML");
